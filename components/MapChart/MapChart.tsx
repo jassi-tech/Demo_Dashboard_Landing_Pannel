a7b6f22df10e72c288c992edddd2d3237ae1bff9
jasleen.singh <jasleen.singh@antiersolutions.com>
2026-02-10 17:54:46 +0530

    project setup

---
+++ components/MapChart/MapChart.tsx
@@ -0,0 +1,102 @@
+'use client';
+
+import React, { useEffect, useMemo, useState } from 'react';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+} from 'react-simple-maps';
+import { csv } from 'd3-fetch';
+import { scaleLinear } from 'd3-scale';
+import sortBy from 'lodash/sortBy';
+import {
+  ActionIcon,
+  Group,
+  Paper,
+  PaperProps,
+  Text,
+  useMantineTheme,
+} from '@mantine/core';
+import { IconDotsVertical } from '@tabler/icons-react';
+import WorldCountriesMap from '@/public/mocks/WorldAtlasCountries.json';
+import { Surface } from '@/components';
+
+const geoUrl = WorldCountriesMap;
+
+type MapChartProps = PaperProps;
+
+const MapChart = ({ ...others }: MapChartProps) => {
+  const theme = useMantineTheme();
+  const [data, setData] = useState<any>([]);
+  const [maxValue, setMaxValue] = useState(0);
+
+  useEffect(() => {
+    //{--- old code ---}
+    // csv('/data.csv').then((cities: any) => {
+    //   const sortedCities = sortBy(cities, (o) => -o.population);
+    //   setMaxValue(sortedCities[0].population);
+    //   setData(sortedCities);
+    // });
+
+    csv('/data.csv').then((cities: any) => {
+      const numeric = cities.map((c: any) => ({
+        ...c,
+        population: +c.population,
+        lng: +c.lng,
+        lat: +c.lat,
+      }));
+      const sorted = sortBy(numeric, (o) => -o.population);
+      setMaxValue(sorted[0]?.population ?? 0);
+      setData(sorted);
+    });
+  }, []);
+
+  const popScale = useMemo(
+    () => scaleLinear().domain([0, maxValue]).range([0, 24]),
+    [],
+  );
+
+  return (
+    <Surface component={Paper} {...others}>
+      <Group justify="space-between" mb="md">
+        <Text size="lg" fw={600}>
+          Real time
+        </Text>
+        <ActionIcon variant="subtle">
+          <IconDotsVertical size={16} />
+        </ActionIcon>
+      </Group>
+      {/*@ts-ignore*/}
+      <ComposableMap projectionConfig={{ rotate: [-10, 0, 0] }}>
+        {/*@ts-ignore*/}
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              // @ts-ignore
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill={theme.colors[theme.primaryColor][1]}
+              />
+            ))
+          }
+        </Geographies>
+        {data.map(({ city_code, lng, lat, population }: any) => {
+          return (
+            // @ts-ignore
+            <Marker key={city_code} coordinates={[lng, lat]}>
+              <circle
+                fill={theme.colors[theme.primaryColor][7]}
+                stroke="#FFF"
+                r={popScale(population)}
+              />
+            </Marker>
+          );
+        })}
+      </ComposableMap>
+    </Surface>
+  );
+};
+
+export default MapChart;
---
+++ setp.md
@@ -0,0 +1,38 @@
+
+```bash
+npm install next@14.1.1 react@18.2.0 react-dom@18.2.0 typescript@5.1.6
+```
+
+### **Mantine v7 Suite**
+```bash 
+npm install @mantine/core@^7.2.2 @mantine/hooks@^7.2.2 @mantine/form@^7.2.2 @mantine/dates@^7.2.2 @mantine/notifications@^7.2.2 @mantine/carousel@^7.2.2 @mantine/tiptap@^7.2.2 @mantine/modals@^7.2.2 @mantine/nprogress@^7.2.2 @mantine/spotlight@^7.2.2 @mantine/dropzone@^7.2.2 @mantine/code-highlight@^7.2.2 @mantine/next@^6.0.16
+```
+
+### **Authentication**
+```bash
+npm install @clerk/nextjs@^4.29.3 @clerk/themes@^1.7.9 next-auth@^4.24.5
+```
+
+### **Charts & Visualization**
+```bash
+npm install apexcharts@^3.44.0 react-apexcharts@^1.4.1 d3-fetch@^3.0.1 d3-scale@^4.0.2 react-simple-maps@^3.0.0
+```
+
+### **Other Utilities & Components**
+```bash
+npm install \
+  @dnd-kit/core@^6.0.8 @dnd-kit/sortable@^7.0.2 @dnd-kit/utilities@^3.2.2 \
+  @fullcalendar/core@^6.1.8 @fullcalendar/daygrid@^6.1.8 @fullcalendar/interaction@^6.1.8 @fullcalendar/react@^6.1.8 @fullcalendar/timegrid@^6.1.8 \
+  @tabler/icons-react@^2.40.0 @tiptap/react@^2.1.12 @tiptap/starter-kit@^2.1.12 \
+  @tiptap/extension-highlight @tiptap/extension-link @tiptap/extension-placeholder @tiptap/extension-subscript @tiptap/extension-superscript @tiptap/extension-text-align @tiptap/extension-underline @tiptap/pm \
+  clsx@^2.0.0 cookies-next@^2.1.2 dayjs@^1.11.10 embla-carousel-react@7.1.0 lodash@^4.17.21 mantine-datatable@^7.1.7 react-countup@^6.4.2 @changesets/cli@2.26.2
+```
+
+### **Dev Dependencies (Tooling & Storybook)**
+```bash
+npm install -D \
+  storybook@7.5.3 @storybook/nextjs@7.5.3 @storybook/react@7.5.3 @storybook/addon-essentials@7.5.3 @storybook/addon-interactions@7.5.3 @storybook/addon-links@7.5.3 @storybook/addon-onboarding@1.0.8 @storybook/addon-styling-webpack@^0.0.5 @storybook/blocks@7.5.3 @storybook/manager-api@^7.5.3 @storybook/testing-library@0.2.2 @storybook/theming@^7.5.3 \
+  postcss@^8.4.31 postcss-preset-mantine@^1.11.0 postcss-simple-vars@^7.0.1 \
+  prettier@3.1.0 @types/node@20.4.4 @types/react@^18.2.37 @types/react-dom@^18.2.15 @types/d3-fetch@^3.0.2 @types/d3-scale@^4.0.3 @types/lodash@^4.14.195 @types/react-simple-maps@^3.0.0 \
+  husky@^8.0.0 lint-staged@^15.1.0 pinst@^3.0.0 chromatic@^9.1.0 @commitlint/cli@^18.4.3 @commitlint/config-conventional@^18.4.3 storybook-dark-mode@^3.0.1
+```
